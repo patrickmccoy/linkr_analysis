e3429ee75f6ab90b97f9847d8cbd3c22a2554997
Patrick McCoy <patrick@pjmccoy.com>
2011-07-26 21:34:06 -0600

    updated the parsing of the log file for easier reading and execution

--- logs/load_to_mongo.py
+++ logs/load_to_mongo.py
@@ -11,47 +11,43 @@ connection = Connection('localhost', 27017)
 
 db = connection.linkr
 
-log_file = open('./production.log')
-
 logs = []
 count = 0
 
-for line in log_file:
-	line = line.rstrip('\n').split('\t')
-	
-	record = {}
-	int_vals = ['status','response_time']
-	strip_quotes = ['user_agent','date']
-	
-	
-	for key_val in line:
-		key_val = key_val.split('=')
-		key = key_val.pop(0)
-		val = key_val.pop()
+with open('./production.log') as f:
+    for line in f:
+		fields = line.rstrip('\n').split('\t')
 		
-		if (key in strip_quotes):
-			val = val.strip('"')
+		record = {}
 		
-		## Parse the date into a datetime object from the following format:
-		## Tue, 26 Jul 2011 22:33:52 GMT
-		if (key == 'date'):
-			val = datetime.strptime(val,'%a, %d %b %Y %H:%M:%S %Z')
+		int_vals = ['status','response_time']
+		strip_quotes = ['user_agent','date']
 		
-		if (key == 'response_time' and val == '-'):
-			val = -1
-		
-		if (key in int_vals):
-			val = int(val)
-		
-		record[key] = val
-		
-	## Push to the logs variable for bulk import
-	logs.append(record)
-	count += 1
-	
+		for field in fields:
+			key,_,val = field.partition('=')
+			
+			# Do something with each key and val
+			
+			if (key in strip_quotes):
+				val = val.strip('"')
+			
+			## Parse the date into a datetime object from the following format:
+			## Tue, 26 Jul 2011 22:33:52 GMT
+			if (key == 'date'):
+				val = datetime.strptime(val,'%a, %d %b %Y %H:%M:%S %Z')
+			
+			if (key == 'response_time' and val == '-'):
+				val = -1
+			
+			if (key in int_vals):
+				val = int(val)
+			
+			record[key] = val
+			
+		## Push to the logs variable for bulk import
+		logs.append(record)
+		count += 1
 
-## Close the log file	
-log_file.close()
 
 ## Insert the new logs into MongoDB
 result = db.logs.insert(logs)
